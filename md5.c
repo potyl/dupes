9a10f787a08cd6e695761ebec147384f95f68af1
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2010-02-20 23:08:58 +0100

    Find the best buffer size by querying fstat

--- md5.c
+++ md5.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/uio.h>
 #include <unistd.h>
 
@@ -36,12 +37,15 @@ char* compute_digest (const char *filename) {
 	unsigned char digest[MD5_DIGEST_LENGTH];
 	char *digest_hex;
 	char *digest_ptr;
-	char buffer[1024];
+	char *buffer;
+	size_t buffer_size;
 	size_t i;
 	ssize_t count;
 	int fd;
+	struct stat stat_data;
+	int ret;
 
-	/* Compute the digest */
+	/* Compute the digest of the file */
 	MD5_Init(&digest_ctx);
 
 	fd = open(filename, O_RDONLY);
@@ -50,15 +54,26 @@ char* compute_digest (const char *filename) {
 		return NULL;
 	}
 
-	while ( (count = read(fd, buffer, sizeof(buffer))) > 0 ) {
+	/* Check what's the file's prefered I/O size */
+	ret = fstat(fd, &stat_data);
+	if (ret != -1) {
+		buffer_size = stat_data.st_blksize;
+	}
+	else {
+		buffer_size = 4096;
+	}
+	buffer = malloc(buffer_size);
+
+	while ( (count = read(fd, buffer, buffer_size)) > 0 ) {
 		MD5_Update(&digest_ctx, buffer, count);
 	}
 	close(fd);
+	free(buffer);
 
 	MD5_Final(digest, &digest_ctx);
 
 	/* Transform the binary digest into a human readable string */
-	digest_hex = (char *) malloc(sizeof(char) * (sizeof(digest) * 2 + 1));
+	digest_hex = (char *) malloc(sizeof(digest) * 2 + 1);
 	digest_ptr = digest_hex;
 	for (i = 0; i < MD5_DIGEST_LENGTH; ++i) {
 		sprintf(digest_ptr, "%02x", digest[i]);
