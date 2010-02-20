0c87520830517db13b790274f7a3c0dc2f2dac51
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2010-02-20 21:47:32 +0100

    Fix compute_digest

--- md5.c
+++ md5.c
@@ -12,6 +12,7 @@ int main (int argc, char *argv[]) {
 	digest = compute_digest("md5.c");
 
 	printf("%s\n", digest);
+	if (digest) {free(digest);}
 
 	return 0;
 }
@@ -19,25 +20,24 @@ int main (int argc, char *argv[]) {
 
 static
 char* compute_digest (const char *filename) {
-	MD5_CTX md5_ctx = {0, };
-	char md5_hex[(MD5_DIGEST_LENGTH * 2) + 1];
+	MD5_CTX md5_ctx;
+	unsigned char digest[MD5_DIGEST_LENGTH];
+	char *digest_hex;
 	char *buffer;
-	unsigned char *digest;
 	size_t i;
 
-	printf("%s\n", filename);
-
+	/* Compute the digest */
 	MD5_Init(&md5_ctx);
 	MD5_Update(&md5_ctx, "1", 1);
-	digest = malloc(sizeof(unsigned char) * (MD5_DIGEST_LENGTH + 1));
 	MD5_Final(digest, &md5_ctx);
 
-	buffer = md5_hex;
+	/* Transform the binary digest into a human readable string */
+	digest_hex = (char *) malloc(sizeof(char) * (sizeof(digest) * 2 + 1));
+	buffer = digest_hex;
 	for (i = 0; i < MD5_DIGEST_LENGTH; ++i) {
-		printf("%2d) %02x\n", i, digest[i]);
 		sprintf(buffer, "%02x", digest[i]);
 		buffer += 2;
 	}
 
-	return buffer;
+	return digest_hex;
 }
