60417dbda0553ceeb279a3023e0d94d3495f03b3
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2010-04-25 21:16:59 +0200

    OS X and Linux friendly

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-aclocal -I /opt/local/share/aclocal
-autoheader
-automake
-autoconf
+# In OS X the ports system installs the aclocal macro in a different location
+includes=
+if [ -e /opt/local/share/aclocal ]; then
+	includes="-I /opt/local/share/aclocal"
+fi
+
+aclocal $includes --force
+autoconf --force
+autoheader --force
+automake --add-missing --copy --force-missing
